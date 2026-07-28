7cd3f12afc1685b011cdb9b07fb0c7a568ba33b8
Ihor Panasiuk <igorskynet13@gmail.com>
2026-07-28 13:55:18 +0200

    fxs

--- elastic_dev_vm/entrypoint.sh
+++ elastic_dev_vm/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Restore SSH authorized_keys (volume mount overwrites /root)
+mkdir -p /root/.ssh && chmod 700 /root/.ssh
+cp /etc/ssh/authorized_keys.bak /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
 # Start Docker daemon in background
 dockerd --host=unix:///var/run/docker.sock &
 
