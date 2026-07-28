9d3ad37719e5bec4ca72040e9691aff8ece277c7
Ihor Panasiuk <igorskynet13@gmail.com>
2026-07-28 23:56:53 +0200

    fx

--- kibana/build-base.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-docker build ^
-  -f kibana/Dockerfile.base ^
-  --build-arg KIBANA_BRANCH=%KIBANA_BRANCH% ^
-  --build-arg KIBANA_REPO_URL=%KIBANA_REPO_URL% ^
-  -t kibana-base:latest ^
-  kibana/
