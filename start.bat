78e68f9ffbe5e60c3989c92139df29bd356c0d7a
Ihor Panasiuk <igorskynet13@gmail.com>
2026-07-30 20:19:58 +0200

    single compose file

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 powershell -Command "$p='elastic_dev_vm\id_ed25519'; $acl=Get-Acl $p; $acl.SetAccessRuleProtection($true,$false); $r=New-Object System.Security.AccessControl.FileSystemAccessRule([System.Security.Principal.WindowsIdentity]::GetCurrent().Name,'Read','Allow'); $acl.SetAccessRule($r); Set-Acl $p $acl" >nul
-docker compose -f elastic_dev_vm/docker-compose.yml up -d --build
+docker compose -f docker-compose.yml up -d --build
