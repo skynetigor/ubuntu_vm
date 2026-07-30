a6c4a686e47a1e314f5169e6b40517e4ecc4c6c3
Ihor Panasiuk <igorskynet13@gmail.com>
2026-07-30 20:25:42 +0200

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 chmod 600 elastic_dev_vm/id_ed25519
-docker compose -f elastic_dev_vm/docker-compose.yml up -d --build
+docker compose -f docker-compose.yml up -d --build
